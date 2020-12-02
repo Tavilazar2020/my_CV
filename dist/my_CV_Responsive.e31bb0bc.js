8711b963115e78726c830d7203f39ecf7e5b85e7
Tavi Lazar <octavian.lazar2013@gmail.com>
2020-12-02 09:59:29 +0100

    braking point at 960

--- dist/my_CV_Responsive.e31bb0bc.js
+++ dist/my_CV_Responsive.e31bb0bc.js
@@ -18678,7 +18678,7 @@ function myFunction(e) {
   }
 }
 
-var x = window.matchMedia("(max-width: 898px)");
+var x = window.matchMedia("(max-width: 960px)");
 myFunction(x); // Call listener function at run time
 
 x.addListener(myFunction); // Attach listener function on state changes
@@ -18711,7 +18711,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60129" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63573" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
